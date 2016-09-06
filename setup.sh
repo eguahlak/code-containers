e06d3fcbd56802fc1cc9a6e00f87cf2962763360
Anders Kalhauge <anders@kalhauge.dk>
2016-09-06 12:43:44 +0200

    Jenkins container with docker v5

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ chown -R 1000:1000 /var/containers/jenkins_home
 
 cd jenkins
 docker build -t eguahlak/jenkins .
-docker run -p 8080:8080 -p 50000:50000 -v /var/containers/jenkins_home:/var/jenkins_home eguahlak/jenkins
+docker run -d -p 8080:8080 -p 50000:50000 -v /var/containers/jenkins_home:/var/jenkins_home eguahlak/jenkins
